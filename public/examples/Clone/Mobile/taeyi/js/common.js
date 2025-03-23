7254f02f22eb07cc2d1addff3ae1f42cef394097
parkJH24 <jh.parkssam@gmail.com>
2025-03-23 17:48:23 +0900

    카카오맵 추가

--- public/examples/Clone/Mobile/taeyi/js/common.js
+++ public/examples/Clone/Mobile/taeyi/js/common.js
@@ -190,4 +190,28 @@ function generateCalendar(year, month, dday) { // ✅ year, month를 매개변�
     }
 }
 
+window.onload = function () {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(37.5665, 126.9780), // 서울 시청 예시
+      level: 3
+    };
+
+    const map = new kakao.maps.Map(container, options);
+
+    // 마커 추가
+    const markerPosition = new kakao.maps.LatLng(37.5665, 126.9780);
+    const marker = new kakao.maps.Marker({
+      position: markerPosition
+    });
+    marker.setMap(map);
+
+    // 인포윈도우 (간단한 설명)
+    const iwContent = '<div style="padding:5px;">돌잔치 장소<br><a href="https://map.kakao.com/link/map/돌잔치장소명,37.5665,126.9780" style="color:blue" target="_blank">큰지도보기</a></div>';
+    const infowindow = new kakao.maps.InfoWindow({
+      content: iwContent
+    });
+    infowindow.open(map, marker);
+  }
+
 
